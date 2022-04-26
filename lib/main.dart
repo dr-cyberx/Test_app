c81078b64c3dbc3a238c6464eee94b1f9c633b60
@vishal.kumar <vishal.kumar@growthnatives.com>
2022-04-26 13:47:02 +0530

    done with the basic business card design

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               CircleAvatar(
                 radius: 50.0,
@@ -22,8 +23,69 @@ class MyApp extends StatelessWidget {
               Text(
                 'John Doe',
                 style: TextStyle(
-                  fontSize: 30.0,
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontFamily: 'SourceSanPro',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 30.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+44 123 456 789',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'SourceSanPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 5.0,
+                  horizontal: 30.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'john_doe@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'SourceSanPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               )
             ],
